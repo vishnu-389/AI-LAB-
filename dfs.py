264706ea5717039394f8521af781ee98729425c2
vishnu-389 <gambirapativishnuvardhan.130180@marwadiuniversity.ac.in>
2026-08-26 17:20:51 +0530

    Add files via upload

---
+++ dfs.py
@@ -0,0 +1,33 @@
+def dfs(graph, node, visited=None):
+    if visited is None:
+        visited = set()
+
+    visited.add(node)
+    print(node, end=" ")
+
+    for neighbor in graph[node]:
+        if neighbor not in visited:
+            dfs(graph, neighbor, visited)
+
+
+graph = {
+    'A': ['B', 'C'],
+    'B': ['D', 'E'],
+    'C': ['F'],
+    'D': [],
+    'E': [],
+    'F': []
+}
+
+dfs(graph, 'A')
+
+
+'''
+      A
+     / \
+    B   C
+   / \  / \
+  D  E F   G
+ /
+H
+'''
