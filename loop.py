0c10b2f841425ede3bb9cfdcfd41f4aabd8fa79f
vishnu-389 <gambirapativishnuvardhan.130180@marwadiuniversity.ac.in>
2026-08-26 16:21:59 +0530

    Add files via upload

---
+++ loop.py
@@ -0,0 +1,10 @@
+question = input("Ask your question: ")
+
+if question == "how much discount i can get":
+    print("Maximum 15% discount.")
+
+elif question == "when will the car get delivered":
+    print("Within 2-3 days.")
+
+else:
+    print("Sorry, I don't understand your question.")
